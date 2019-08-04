d1f6fbd4ebb49f21ce6a42ab8451377df22d3dc5
Jose Carlos Alvarez <jalvarez2153@universidadean.edu.co>
2019-08-04 12:27:13 -0500

    update1

---
+++ colectivo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Programa para hallar viajes, ganancia y costo
+ * 
+ * @author Carlos Alvarez 
+ * @version 20190803
+ */
+public class colectivo
+{
+    public static void main(String[] args)
+    {
+        double n,cap=50,tk=10000,cost=2000,viaj,gan,prop;
+        
+        Scanner Teclado = new Scanner (System.in);
+        
+        System.out.println("El valor del pasaje es de 10.000 CoP, existe capacidad para 50 personas");
+        
+        System.out.println("Cuantas personas viajan en el colectivo");
+        n = Teclado.nextDouble();
+        
+        viaj = Math.ceil(n/cap);
+        gan = n*10000;
+        prop = n*2000;
+                 
+        System.out.println("La cantidad de viajes es " + viaj);
+        System.out.println("Todos los turistas pagaron " + "$" + gan + "CoP");
+        System.out.println("El conductor paga al propietario " + "$" + prop + "CoP");
+
+    }
+}
